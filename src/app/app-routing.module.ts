2230a33a4cdcf7e1c3ca712149ec4f66b0090b02
mireyachaparro <mireyacn99@gmail.com>
2023-03-07 20:26:45 +0100

    Create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeclaracionesComponent } from './declaraciones/declaraciones.component';
+import { FinanciacionComponent } from './financiacion/financiacion.component';
+import { IncidenciasComponent } from './incidencias/incidencias.component';
+import { InformesComponent } from './informes/informes.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { InstalacionesComponent } from './instalaciones/instalaciones.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'declaraciones', component: DeclaracionesComponent },
+  { path: 'instalaciones', component: InstalacionesComponent },
+  { path: 'incidencias', component: IncidenciasComponent },
+  { path: 'financiacion', component: FinanciacionComponent },
+  { path: 'informes', component: InformesComponent },
+  { path: '**', component: InicioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
