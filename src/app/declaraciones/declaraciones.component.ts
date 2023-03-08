e1fe8212f2529077b5efb1ec7ba6d7234bf87e16
mireyachaparro <mireyacn99@gmail.com>
2023-03-08 01:47:25 +0100

    Create declaraciones page

---
+++ src/app/declaraciones/declaraciones.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-declaraciones',
+  templateUrl: './declaraciones.component.html',
+  styleUrls: ['./declaraciones.component.scss']
+})
+export class DeclaracionesComponent {
+
+}
---
+++ src/app/declaraciones/declaraciones.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DeclaracionesComponent } from './declaraciones.component';
+
+
+
+@NgModule({
+  declarations: [
+    DeclaracionesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DeclaracionesModule { }
