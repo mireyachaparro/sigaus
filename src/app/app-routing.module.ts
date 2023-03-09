d31bf638d9c512323c042448e98aef0cc0711ac0
mireyachaparro <mireyacn99@gmail.com>
2023-03-09 19:58:12 +0100

    Add incidencias to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { DeclaracionesComponent } from './declaraciones/declaraciones.component';
 import { InstalacionesComponent } from './instalaciones/instalaciones.component';
+import { IncidenciasComponent } from './incidencias/incidencias.component';
 // import { FinanciacionComponent } from './financiacion/financiacion.component';
-// import { IncidenciasComponent } from './incidencias/incidencias.component';
 // import { InformesComponent } from './informes/informes.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'declaraciones', component: DeclaracionesComponent },
   { path: 'instalaciones', component: InstalacionesComponent },
-  // { path: 'incidencias', component: IncidenciasComponent },
+  { path: 'incidencias', component: IncidenciasComponent },
   // { path: 'financiacion', component: FinanciacionComponent },
   // { path: 'informes', component: InformesComponent },
   { path: '**', component: InicioComponent },
