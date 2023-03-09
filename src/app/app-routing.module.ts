d755e67b2c6e172070fc88625e306144c9bfb25d
mireyachaparro <mireyacn99@gmail.com>
2023-03-09 19:52:12 +0100

    Add instalaciones to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { DeclaracionesComponent } from './declaraciones/declaraciones.component';
+import { InstalacionesComponent } from './instalaciones/instalaciones.component';
 // import { FinanciacionComponent } from './financiacion/financiacion.component';
 // import { IncidenciasComponent } from './incidencias/incidencias.component';
 // import { InformesComponent } from './informes/informes.component';
 
-// import { InstalacionesComponent } from './instalaciones/instalaciones.component';
-
 const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'declaraciones', component: DeclaracionesComponent },
-  // { path: 'instalaciones', component: InstalacionesComponent },
+  { path: 'instalaciones', component: InstalacionesComponent },
   // { path: 'incidencias', component: IncidenciasComponent },
   // { path: 'financiacion', component: FinanciacionComponent },
   // { path: 'informes', component: InformesComponent },
